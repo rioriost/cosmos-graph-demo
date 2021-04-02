756b4b3f3c49e2034367e63189d49e84eba803c6
Rio Fujita <rio_github@rio.st>
2021-04-02 11:24:26 +0900

    Add files via upload

---
+++ airline.py
@@ -0,0 +1,142 @@
+from gremlin_python.driver import client, serializer, protocol
+from gremlin_python.driver.protocol import GremlinServerError
+import sys
+import traceback
+import csv
+
+_gremlin_cleanup_graph = "g.V().drop()"
+
+_gremlin_count_vertices = "g.V().count()"
+
+def print_status_attributes(result):
+    # This logs the status attributes returned for successful requests.
+    # See list of available response status attributes (headers) that Gremlin API can return:
+    #     https://docs.microsoft.com/en-us/azure/cosmos-db/gremlin-headers#headers
+    #
+    # These responses includes total request units charged and total server latency time.
+    # 
+    # IMPORTANT: Make sure to consume ALL results returend by cliient tothe final status attributes
+    # for a request. Gremlin result are stream as a sequence of partial response messages
+    # where the last response contents the complete status attributes set.
+    #
+    # This can be 
+    print("\tResponse status_attributes:\n\t{0}".format(result.status_attributes))
+
+def cleanup_graph(client):
+    print("\n> {0}".format(
+        _gremlin_cleanup_graph))
+    callback = client.submitAsync(_gremlin_cleanup_graph)
+    if callback.result() is not None:
+        callback.result().all().result() 
+    print("\n")
+    print_status_attributes(callback.result())
+    print("\n")
+
+
+def insert_vertices(client):
+    with open('air-routes-latest-nodes.csv', newline='') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            query = "g.addV('airport').property('id', row[0]).property('code', row[3]).property('icao', row[4]).property('desc', row[5])"
+            print("\n> {0}\n".format(query))
+            callback = client.submitAsync(query)
+            if callback.result() is not None:
+                print("\tInserted this vertex:\n\t{0}".format(
+                    callback.result().all().result()))
+            else:
+                print("Something went wrong with this query: {0}".format(query))
+            print("\n")
+            print_status_attributes(callback.result())
+            print("\n")
+
+    print("\n")
+
+
+def insert_edges(client):
+    with open('air-routes-latest-edges.csv', newline='') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            query = "g.V(row[1]).addE('line').to(g.V(row[2]))"
+            print("\n> {0}\n".format(query))
+            callback = client.submitAsync(query)
+            if callback.result() is not None:
+                print("\tInserted this edge:\n\t{0}\n".format(
+                    callback.result().all().result()))
+            else:
+                print("Something went wrong with this query:\n\t{0}".format(query))
+            print_status_attributes(callback.result())
+            print("\n")
+
+    print("\n")
+
+
+def count_vertices(client):
+    print("\n> {0}".format(
+        _gremlin_count_vertices))
+    callback = client.submitAsync(_gremlin_count_vertices)
+    if callback.result() is not None:
+        print("\tCount of vertices: {0}".format(callback.result().all().result()))
+    else:
+        print("Something went wrong with this query: {0}".format(
+            _gremlin_count_vertices))
+
+    print("\n")
+    print_status_attributes(callback.result())
+    print("\n")
+
+
+try:
+    client = client.Client('wss://<YOUR_ENDPOINT>.gremlin.cosmosdb.azure.com:443/', 'g',
+                           username="/dbs/<YOUR_DATABASE>/colls/<YOUR_COLLECTION_OR_GRAPH>",
+                           password="<YOUR_PASSWORD>",
+                           message_serializer=serializer.GraphSONSerializersV2d0()
+                           )
+
+    print("Welcome to Azure Cosmos DB + Gremlin on Python!")
+
+    # Drop the entire Graph
+    input("We're about to drop whatever graph is on the server. Press any key to continue...")
+    cleanup_graph(client)
+
+    # Insert all vertices
+    input("Let's insert some vertices into the graph. Press any key to continue...")
+    insert_vertices(client)
+
+    # Create edges between vertices
+    input("Now, let's add some edges between the vertices. Press any key to continue...")
+    insert_edges(client)
+
+    # Count all vertices
+    input("Okay. Let's count how many vertices we have. Press any key to continue...")
+    count_vertices(client)
+
+
+except GremlinServerError as e:
+    print('Code: {0}, Attributes: {1}'.format(e.status_code, e.status_attributes))
+
+    # GremlinServerError.status_code returns the Gremlin protocol status code
+    # These are broad status codes which can cover various scenaios, so for more specific
+    # error handling we recommend using GremlinServerError.status_attributes['x-ms-status-code']
+    # 
+    # Below shows how to capture the Cosmos DB specific status code and perform specific error handling.
+    # See detailed set status codes than can be returned here: https://docs.microsoft.com/en-us/azure/cosmos-db/gremlin-headers#status-codes
+    #
+    # See also list of available response status attributes that Gremlin API can return:
+    #     https://docs.microsoft.com/en-us/azure/cosmos-db/gremlin-headers#headers
+    cosmos_status_code = e.status_attributes["x-ms-status-code"]
+    if cosmos_status_code == 409:
+        print('Conflict error!')
+    elif cosmos_status_code == 412:
+        print('Precondition error!')
+    elif cosmos_status_code == 429:
+        print('Throttling error!');
+    elif cosmos_status_code == 1009:
+        print('Request timeout error!')
+    else:
+        print("Default error handling")
+
+    traceback.print_exc(file=sys.stdout) 
+    sys.exit(1)
+
+print("\nAnd that's all! Sample complete")
+input("Press Enter to continue...")
